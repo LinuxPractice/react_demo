611cac535f10cf1f71c7e5505571bc61c337923f
LinuxPractice <william.john.platt@gmail.com>
2018-09-16 01:21:27 -0400

    test
    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import './bootstrap.min.css';
 import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
